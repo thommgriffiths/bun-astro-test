8f2f643f8438baac9ce4c7a7816c04dce9b935a3
thommgriffiths <50466149+thommgriffiths@users.noreply.github.com>
2023-09-13 20:44:31 +0000

    test adding react component

---
+++ test-astro-bun/src/components/MiComponente.jsx
@@ -0,0 +1,15 @@
+import React from react
+
+const MiComponente = () => {
+
+    return(
+        <>
+        <div>
+            <h1>Este es mi componete react</h1>
+            <p>es muy lindo</p>
+        </div>
+        </>
+    )
+}
+
+export default MiComponente
