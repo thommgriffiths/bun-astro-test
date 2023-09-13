11fbbf8590067b81a4b801f2c2a7c1fcc978a989
thommgriffiths <50466149+thommgriffiths@users.noreply.github.com>
2023-09-13 20:54:38 +0000

    test agregar un componente dinamico

--- test-astro-bun/src/components/MiComponente.jsx
+++ test-astro-bun/src/components/MiComponente.jsx
@@ -1,12 +1,21 @@
-import React from "react"
+import React, {useEffect, useState} from "react"
 
 const MiComponente = () => {
+    const [contador, setContador] = useState(0)
+
+    useEffect(() => {
+        setInterval(() => {
+           setContador(contador + 1)
+        }, 1000)
+    }, [])
+
 
     return(
         <>
         <div>
             <h1>Este es mi componete react</h1>
             <p>es muy lindo</p>
+        <p>Ademas se actualizo dinamicamente {contador} veces!</p>
         </div>
         </>
     )
